879602b569dc9865823fc1b2616b2ac269659069
Satyam <Satyam@example>
2022-07-17 21:08:43 +0530

    satyam

--- roboshop/component/forntend.sh
+++ roboshop/component/forntend.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-set -e 
-
+set -e
+COMPONENT=frontend
+LOGFILE="/tmp/$COMPONENT.LOG" 
 source component/common.sh
 
-yum install nginx -y
-systemctl enable nginx
+stat(){
+	if [$1 -eq 0] ; then
+    	echo -n "\e[32m SUCCESS \e[0m"
+	else
+		echo -n "\e[32m  FAILURE \e[0m"
+	fi
+}
+echo -n "Installing the nginx"
+yum install nginx -y &>>LOGFILE
+stat $?
+systemctl enable nginx &>>LOGFILE
+
+echo -n "Starting the nginx"
 systemctl start nginx
+stat $?
+echo -n "Dwonloading and extracting the file $COMPONENT"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
