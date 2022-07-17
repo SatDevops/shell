401d35f7a5cca5f00a29920e6fd440ea41e79a04
Satyam <Satyam@example>
2022-07-17 16:42:10 +0530

    satyam

--- roboshop/component/forntend.sh
+++ roboshop/component/forntend.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-yum install nginxfdnklfsn -y
+yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
